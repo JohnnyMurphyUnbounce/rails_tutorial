2ad93ceac3545a9c94ce941596ffd0dab370ceac
JohnnyMurphyUnbounce <johnnymurphy@johnny-murphy.local>
2016-12-09 11:25:07 -0800

    updated hello text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "¡Hola, mundo!"
+  	render html: "¡Hola, mundo! AKA hello world"
   end
 
   def goodbye
